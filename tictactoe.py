e02dc52c56449ee03d087b70dcdb458c7ebd3ff7
claire <c.justliketheword@gmail.com>
2023-06-15 14:27:47 -0700

    added comments and 1p mode

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,20 @@
 #! /usr/bin/env python3
 
 from tkinter import *
+import random
+import time
 
 board = ["","","","","","","","",""]
 MAGIC_SQUARE = [4, 9, 2, 3, 5, 7, 8, 1, 6]
 
 player = True
 over = False
+mode = "2P"
 
 def check_win(player):
+    """
+    Uses a magic square to determine if the player has won. Also can return whether the game is a tie because might as well kill 2 birds with one stone
+    """
     count = 0
     for x in range(9):
         for y in range(9):
@@ -22,34 +28,74 @@ def check_win(player):
             count += 1
         if count == 9:
             return "tie"
-                        
+
 def click(i):
+    """
+    makes the game buttons do stuff depending on whether the space is available and what mode the game is on
+    """
     global over
-    global player
     if over:
         return
     if board[i] == "":
-        if player:
-            board[i] = "X"
-            buttons[i].configure(text = "X", fg = "green")
-            text.configure(text = "O to play")
-            player = not player
-            if check_win("X"):
-                over = True
-                text.configure(text = "X WINS!", fg = "green")
-        else:
-            board[i] = "O"
-            buttons[i].configure(text = "O", fg = "cyan")
-            text.configure(text = "X to play")
-            player = not player
-            if check_win("O"):
-                over = True
-                text.configure(text = "O WINS!", fg = "cyan")
+        if mode == "2P":
+            two_player_turn(i)
+        elif mode == "easy":
+            ai_turn_easy(i)
+        elif mode == "hard":
+            pass 
+            #ai_turn_hard(i) to implement
     if type(check_win("X")) == str:
         over = True
         text.configure(text = "TIE", fg = "red")
 
+def two_player_turn(i):
+    global player
+    global over
+    if player:
+        board[i] = "X"
+        buttons[i].configure(text = "X", fg = "green")
+        text.configure(text = "O to play")
+        player = not player
+        if check_win("X"):
+            over = True
+            text.configure(text = "X WINS!", fg = "green")
+    else:
+        board[i] = "O"
+        buttons[i].configure(text = "O", fg = "cyan")
+        text.configure(text = "X to play")
+        player = not player
+        if check_win("O"):
+            over = True
+            text.configure(text = "O WINS!", fg = "cyan")
+
+
+def ai_turn_easy(i):
+    global over
+    board[i] = "X"
+    buttons[i].configure(text = "X", fg = "green")
+    if check_win("X"):
+        over = True
+        text.configure(text = "X WINS!", fg = "green")
+        return
+    if type(check_win("X")) == str:
+        over = True
+        text.configure(text = "TIE", fg = "red")
+        return
+    while True:
+        x = random.randint(0, 8)
+        if board[x] == "":
+            board[x] = "O"
+            buttons[x].configure(text = "O", fg = "cyan")
+            if check_win("O"):
+                over = True
+                text.configure(text = "O WINS!", fg = "green")
+                return
+            break
+
 def reset():
+    """
+    resets everything to default values so the program doesn't become useless when a game ends
+    """
     global over
     global player
     global board
@@ -61,19 +107,48 @@ def reset():
     text.configure(text = "X to play", fg = "white")
 
 def close():
+    """
+    just adds a close game button. nothing special
+    """
     game.destroy()
         
+"""
+mode switching!!!!
+"""
+def twoPlayer():
+    reset()
+    global mode
+    mode = "2P"
+
+def easyMode():
+    reset()
+    global mode
+    mode = "easy"
+
+#TO IMPLEMENT
+"""def hardmode(): 
+    reset()
+    global mode
+    mode = "hard"
+"""
+
+"""
+creates game board
+"""
 game = Tk(className = "Tic Tac Toe")
-game.geometry("600x700")
+game.geometry("600x800")
 game.configure(bg = "#1a1a1a")
 game.title("Tic Tac Toe")
-game.minsize(400, 450)
+game.minsize(400, 550)
 
 photo = PhotoImage(file = "tictactoe_large.png")
 game.iconphoto(False, photo)
 
 buttons = []
 
+"""
+this could probably be done in a loop but it didn't work for me, kept passing 9 to the lambda? idk
+"""
 buttons.append(Button(game, text = "", font = ("Arial", 40), bg = "#1f1f1f", fg = "white", anchor = "center", width = 10, command = lambda: click(0)))
 buttons.append(Button(game, text = "", font = ("Arial", 40), bg = "#1f1f1f", fg = "white", anchor = "center", width = 10, command = lambda: click(1)))
 buttons.append(Button(game, text = "", font = ("Arial", 40), bg = "#1f1f1f", fg = "white", anchor = "center", width = 10, command = lambda: click(2)))
@@ -84,6 +159,9 @@ buttons.append(Button(game, text = "", font = ("Arial", 40), bg = "#1f1f1f", fg 
 buttons.append(Button(game, text = "", font = ("Arial", 40), bg = "#1f1f1f", fg = "white", anchor = "center", width = 10, command = lambda: click(7)))
 buttons.append(Button(game, text = "", font = ("Arial", 40), bg = "#1f1f1f", fg = "white", anchor = "center", width = 10, command = lambda: click(8)))
 
+"""
+adds the bottom row UI elements
+"""
 text = Label(game, text = "X to play", bg = "#1a1a1a", fg = "white", anchor = "center", font = ("Arial", 20))
 text.grid(row = 3, column = 1, sticky = "EW", padx = 5, pady = 5)
 
@@ -93,6 +171,15 @@ reset_button.grid(column = 2, row = 3)
 close_button = Button(game, text = "Quit", bg = "#1f1f1f", fg = "red", anchor = "center", width = 10, command = close)
 close_button.grid(column = 0, row = 3)
 
+twoPlayer_button = Button(game, text = "2 Player", bg = "#1f1f1f", fg = "white", anchor = "center", width = 10, command = twoPlayer)
+twoPlayer_button.grid(column = 0, row = 4)
+
+easy_button = Button(game, text = "1P Easy", bg = "#1f1f1f", fg = "white", anchor = "center", width = 10, command = easyMode)
+easy_button.grid(column = 1, row = 4)
+
+"""
+also maybe could be done in a loop i just didnt feel like figuring out how to do it. will implement later
+"""
 buttons[0].grid(column = 0, row = 0, sticky = "NSEW")
 buttons[1].grid(column = 1, row = 0, sticky = "NSEW")
 buttons[2].grid(column = 2, row = 0, sticky = "NSEW")
@@ -103,10 +190,14 @@ buttons[6].grid(column = 0, row = 2, sticky = "NSEW")
 buttons[7].grid(column = 1, row = 2, sticky = "NSEW")
 buttons[8].grid(column = 2, row = 2, sticky = "NSEW")
 
+"""
+forces grid spaces to stay consistent with each other so the game looks pretty
+"""
 game.grid_rowconfigure(0, weight = 2)
 game.grid_rowconfigure(1, weight = 2)
 game.grid_rowconfigure(2, weight = 2)
 game.grid_rowconfigure(3, weight = 1)
+game.grid_rowconfigure(4, weight = 1)
 game.grid_columnconfigure(0, weight = 1)
 game.grid_columnconfigure(1, weight = 1)
 game.grid_columnconfigure(2, weight = 1)
